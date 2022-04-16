560c398d9de13b3d389f323f9dde318064755503
tammyafnan <aysha.afnan@g.bracu.ac.bd>
2022-04-17 02:57:30 +0600

    Initial commit

---
+++ application/views/mobile/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<?php include 'includes_top.php'; ?>
+</head>
+
+<body>
+	<div class="container-fluid">
+		<!-- HEADER -->
+		<?php include $page_name . '.php'; ?>
+	</div>
+	<!-- all the js files -->
+	<?php include 'includes_bottom.php'; ?>
+	<?php include 'payment_model.php'; ?>
+</body>
+
+</html>

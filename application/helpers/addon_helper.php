4b701365c4d00a137b9a717236561fba3dc84858
tammyafnan <63313492+tammyafnan@users.noreply.github.com>
2022-05-02 12:58:37 +0600

    Update addon_helper.php

--- application/helpers/addon_helper.php
+++ application/helpers/addon_helper.php
@@ -1,17 +1,4 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-/**
-* CodeIgniter
-*
-* An open source application development framework for PHP 5.1.6 or newer
-*
-* @package		CodeIgniter
-* @author		ExpressionEngine Dev Team
-* @copyright	Copyright (c) 2008 - 2011, EllisLab, Inc.
-* @license		http://codeigniter.com/user_guide/license.html
-* @link		http://codeigniter.com
-* @since		Version 1.0
-* @filesource
-*/
 
 /* CHECK THE ADDON STATUS */
 if (! function_exists('addon_status')) {
@@ -75,6 +62,3 @@ if (! function_exists('get_pending_offline_payment')) {
 
 
 
-// ------------------------------------------------------------------------
-/* End of file addon_helper.php */
-/* Location: ./system/helpers/common.php */
